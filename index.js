aa7859ca3b5e575dad13c45370373c78809e2929
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2022-01-18 21:56:28 +0100

    basic drumkit vanila js

---
+++ index.js
@@ -0,0 +1,53 @@
+const date = new Date();
+let year = date.getFullYear();
+
+const yearspan = document.querySelector('.year');
+yearspan.innerHTML = year;
+
+const btnClicked = document.querySelectorAll(".drum");
+
+btnClicked.forEach((btn) => {
+    btn.addEventListener('click', (e) => {  
+        
+        //interaction
+        animateBtns(btn);
+
+        playSound(e.target.dataset.sound);
+
+    });
+})
+
+//keys 
+document.addEventListener('keydown', keyElem => {
+  
+    
+    if(keyElem.code.includes('Key')) {
+        const elementToPlay = document.querySelector(`.${keyElem.key}`);
+
+        if(elementToPlay) {
+            //interaction 
+            animateBtns(elementToPlay);
+
+            //play the sound
+            elementToPlay ? playSound(elementToPlay.dataset.sound) : 'Nothing to play';
+        }
+
+    }
+    
+
+});
+
+const animateBtns = btn => {
+    btn.style.transform = "scale(1.2)";
+    btn.style.borderColor = "#ffe137";
+    setTimeout(()=> {
+        btn.style.transform = "scale(1)";
+        btn.style.borderColor = "#3c0c32";
+    }, 100);
+}
+
+const playSound = sound => {
+    const playIt = new Audio(`./sounds/${sound}.mp3`);
+    playIt.play();
+}
+
